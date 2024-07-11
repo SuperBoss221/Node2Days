40affb88b1c2c8eb3717477bf25bb7ee392a28f2
SuperBoss221 <Superbos.211@gmail.com>
2024-07-11 15:18:53 +0700

    connect

--- connect.js
+++ connect.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: "localhost",
+  host: atob("bG9jYWxob3N0"),
   user: "root",
   database: "member",
   waitForConnections: true,
